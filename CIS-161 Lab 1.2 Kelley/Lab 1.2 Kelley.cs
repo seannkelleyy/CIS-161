9720671e74a17aec33be7a17264c76e9a6ee8cd6
Sean Kelley <94130791+seannkelleyy@users.noreply.github.com>
2022-03-31 18:21:36 -0400

    Add files via upload

---
+++ CIS-161 Lab 1.2 Kelley/Lab 1.2 Kelley.cs
@@ -0,0 +1,20 @@
+﻿/*
+ * lab 1.2
+ * Sean Kelley
+ * 30 January 2022
+ */
+
+using static System.Console;
+
+class Name
+{
+    static void Main()
+    {
+        WriteLine(" SSSS    K   K ");
+        WriteLine(" S       K  K  ");
+        WriteLine(" SSSS    K K   ");
+        WriteLine("    S    K  K  ");
+        WriteLine(" SSSS    K   K ");
+    }
+
+}
