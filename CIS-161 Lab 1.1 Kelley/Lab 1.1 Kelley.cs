673354d12c29f00ecf1aa4f050eec3a81fc64705
Sean Kelley <94130791+seannkelleyy@users.noreply.github.com>
2022-03-31 17:24:06 -0400

    Add files via upload

---
+++ CIS-161 Lab 1.1 Kelley/Lab 1.1 Kelley.cs
@@ -0,0 +1,16 @@
+﻿/*
+ * lab 1.1
+ * Sean Kelley
+ * 29 January 2022
+ */
+
+using static System.Console;
+
+class Hello
+{
+    static void LabOneOne()
+    {
+        WriteLine("Hello World!");
+    }
+    
+}
